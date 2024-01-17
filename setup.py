6b18fc36872b47e35aafb59937f96c411653d235
Radim Sückr <radim.suckr@skippay.cz>
2024-01-17 16:16:56 +0100

    chore: remove author section
    Irrelevant contact

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
     long_description_content_type='text/markdown',
     version='0.1.1.3',
     description="Library contains django commands which helps to prepare and manage PostgreSQL database.",
-    author='Lubos Matl',
-    author_email='matllubos@gmail.com',
     url='https://github.com/skip-pay/django-psycopg2-extension',
     license='MIT',
     package_dir={'psycopg2_extension': 'psycopg2_extension'},
